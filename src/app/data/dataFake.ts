7f5df26ea03a78d8d5bb6cb2f8d97702e9ff0b9c
Victor Matheus <victormatheus507@gmail.com>
2024-02-23 17:36:10 -0300

    content page dinamico

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,14 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": '',
-    "title": '',
-    "description": ''
+    "id": '1',
+    "photo": 'https://tse4.mm.bing.net/th?id=OIP.u3mgmYMq4ofTd4lBtZtbxQHaD4&pid=Api&P=0&h=180',
+    "title": 'Novas novidades do Angular',
+    "description": 'Foi divulgado novas versões do angular, teste agora!!'
+  },
+  {
+    "id": '2',
+    "photo": 'https://tse1.mm.bing.net/th?id=OIP.-jwubw23t4XMn315eOb4_QHaDt&pid=Api&P=0&h=180',
+    "title": 'Saiu a nova versão do angular',
+    "description": 'Nesse ultimo final de semana saiu a ulltima versão do Angular'
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgComponentOutlet],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
@@ -12,10 +14,25 @@ export class ContentComponent {
   photoCover: string = 'https://tse1.mm.bing.net/th?id=OIP.BypcP5XhB3QepH9v4DmuGAHaEK&pid=Api&P=0&h=180'
   contentTitle: string = 'Minha notícia'
   contentDescription:string = 'Ola mundo!'
+  private id: string | null = '0'
 
   constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get('id'))
+      this.id = value.get('id')
     )
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article => article.id === id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photo
+
   }
 }
